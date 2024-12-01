72ffb67424a9f710c301ca47522157240692ecd9
Samuel <samulomba96@gmail.com>
2024-12-01 19:34:53 +0100

    modificato il menu principale

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -7,7 +7,9 @@ import { Component } from '@angular/core';
 })
 export class HeaderComponent {
 
-  helpMenuItems = [
+  //MENU DI AIUTO
+  //da implementare
+  subMenuHelpItems = [
     'Aiuto',
     'Stato ordine',
     'Spedizione e consegna',
@@ -20,12 +22,58 @@ export class HeaderComponent {
     'Invia feedback'
   ];
 
-  // navMenuItems = [
-  //   'Novità e in evidenza',
-  //   'Uomo',
-  //   'Donna',
-  //   'Kids',
-  //   'Offerte'
-  // ];
+  //da implementare
+  helpMenuItems = [
+    'Trova un negozio',
+    'Aiuto',
+    'Unisciti a noi',
+    'Accedi' 
+  ];
 
+  //MENU NAV PRINCIPALE 
+  primoSubMenuPrincipale = [
+    'In evidenza',
+    'Tutti i nuovi arrivi',
+    'Best seller',
+    'Member Shop',
+    'Calendario dei lanci SNKRS',
+    'Shop Air',
+    'Summer of sport',
+    'I must-have per il rientro a scuola',
+  ];
+
+  secondoSubMenuPrincipale = [
+    'Le icone',
+    'Air Force 1',
+    'Air Jordan 1',
+    'Air Max',
+    'Dunk',
+    'Blazer',
+    'Pegasus',
+    'Mercurial',
+  ];
+
+  terzoSubMenuPrincipale = [
+    'Scopri lo sport',
+    'Calcio',
+    'Running',
+    'Basket',
+    'Fitness',
+    'Golf',
+    'Tennis',
+    'Yoga',
+    'Danza',
+    'Skateboard',
+  ];
+
+  quartoSubMenuPrincipale = [
+    'Di tendenza',
+    'Air Max Home',
+    'Sneakers Y2K',
+    'Nike Style By',
+    'Teenager',
+    'EasyOn',
+    'Idee regalo Nike',
+    'Sostenibilità',
+  ];
 }
